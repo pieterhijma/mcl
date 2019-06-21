02e2ca3ddb6046e52354cce37ff69a80e2c4278f
Pieter Hijma <pieter@cs.vu.nl>
2019-06-21 16:59:09 +0200

    Add a Xeon hardware and codegen description

---
+++ input/codegen/xeon_e5620.lisp
@@ -0,0 +1,11 @@
+'(:target "xeon_e5620"
+  :output "opencl"
+  :device-type "CL_DEVICE_TYPE_CPU"
+  :memory-spaces (("reg" . ("" ""))
+		  ("main" . ("__global" "barrier(CLK_GLOBAL_MEM_FENCE)")))
+  :pargroups (("vectors" . ("get_local_id" "get_local_size"))
+	      ("threads" . ("get_group_id" "get_group_size")))
+  :dimensions (("get_local_id" . (3 :func))
+	       ("get_group_id" . (3 :func)))
+  :bandwidth-memoryspace "main"
+  :builtin-funcs (("atomicAdd" . "atomic_add")))
